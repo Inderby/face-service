fd055f2623eba72673ecf809c8fb152845ec6603
Inderby <youk8253@gmail.com>
2023-07-23 17:34:51 +0900

    #2 - 스프링부트 프로젝트 시작
    *스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ wellibee/src/main/java/com/example/wellibee/WellibeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.wellibee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellibeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellibeeApplication.class, args);
+    }
+
+}
