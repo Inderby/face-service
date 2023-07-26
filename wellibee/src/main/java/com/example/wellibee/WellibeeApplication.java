4209c08ec1b49bd814e084eaff61dd1ab54df5d4
Inderby <youk8253@gmail.com>
2023-07-26 16:37:41 +0900

    #10 - Jascypt를 이용한 암호화
    * Jascypt를 이용해 앞으로 추가될 AWS 클라우드의 액세스 키와 시크릿 키를 암호화 하였다. 복호화에 대한 password는 IDE의 환경변수로 설정해 놨기 때문에 git 레포지토리에 올라가지 않는다.

--- wellibee/src/main/java/com/example/wellibee/WellibeeApplication.java
+++ wellibee/src/main/java/com/example/wellibee/WellibeeApplication.java
@@ -1,9 +1,19 @@
 package com.example.wellibee;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableMongoRepositories("com.example.wellibee.repository")
+@ComponentScan({
+        "com.example.wellibee.*"
+})
+@EnableEncryptableProperties
+@EnableWebMvc
 public class WellibeeApplication {
 
     public static void main(String[] args) {
